1eb5521a81b603ab802c4b67df04c06e4d00c4ce
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-27 11:20:18 -0500

    Correct filter test

--- mylist.rb
+++ mylist.rb
@@ -1,25 +1,28 @@
 require_relative './enumerable'
 
+
 class MyList
-  include MyEnumerable
-  def initialize(*args)
-    @list = args
+    include MyEnumerable
+    def initialize(*args)
+      @list = args
+    end
+  
+    def each(&block)
+      @list.each(&block)
+    end
   end
-
-  def each(&block)
-    @list.each(&block)
-  end
-end
-
-# Create our list
-list = MyList.new(1, 2, 3, 4)
-
-# Test #all?
-puts(list.all? { |e| e < 5 }) # true
-puts(list.all? { |e| e > 5 }) # false
-
-# Test #any?
-puts(list.any? { |e| e == 2 }) # true
-puts(list.any? { |e| e == 5 }) # false
-
-puts(list.filter(&:even)) # [2, 4]
+  
+  # Create our list
+  list = MyList.new(1, 2, 3, 4)
+  
+  # Test #all?
+  puts(list.all? { |e| e < 5 }) # true
+  puts(list.all? { |e| e > 5 }) # false
+  
+  # Test #any?
+  puts(list.any? { |e| e == 2 }) # true
+  puts(list.any? { |e| e == 5 }) # false
+  
+  # Test #filter
+  print(list.filter { |e| e.even? }) # [2, 4]
+  
