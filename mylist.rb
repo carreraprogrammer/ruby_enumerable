aac2c0b5c26cef50d035b6afa7c48839777d1565
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2023-01-27 11:06:58 -0500

    Add enumerable

---
+++ mylist.rb
@@ -0,0 +1,25 @@
+require_relative './my_enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+puts(list.filter(&:even)) # [2, 4]
